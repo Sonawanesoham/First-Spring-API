efe3cf2549e435390d9d717927f636f522817a88
Soham <sonawanesoham2002@gmail.com>
2023-08-18 18:49:09 +0530

    First Spring API

---
+++ src/main/java/com/example/studentManagmentapp/StudentManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentManagmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentAppApplication.class, args);
+	}
+
+}
